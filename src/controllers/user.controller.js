434599d281a309adcbfd6cf1442b03d15cbc4891
mohan13 <mohangrg250@gmail.com>
2024-02-16 10:11:06 +0545

    added controller and bug fixxes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,52 @@
+import { ApiError } from '../utils/ApiError.js';
 import { asyncHandler } from '../utils/asynHandlers.js';
+import { User } from '../models/user.model.js';
+import { uploadOnCloudinary } from '../utils/cloudenry.js';}
+import { ApiResponse } from '../utils/ApiRespones.js';}
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: 'ok',
+  const { fullName, email, username, password } = req.body;
+  console.log('email', email);
+  if ([fullName, email, username, password].some((field) => field?.trim() === '')) {
+    throw new ApiError(400, 'All fields is required');
+  }
+  const existedUser = User.findOne({
+    // check user availablity
+    $or: [{ username }, { email }],
   });
+  if (existedUser) {
+    throw new ApiError(409, 'User with email or username already exists');
+  }
+  const avataLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if (!avataLocalPath) {
+    throw new ApiError(400, 'Avatar file is required');
+  }
+ const avatar= await uploadOnCloudinary(avataLocalPath)
+ const coverImage= await uploadOnCloudinary(coverImageLocalPath)
+
+ if(!avatar){
+  throw new ApiError(400,"avatar is required!")
+}
+
+User.create({
+  fullName,
+  avatar:avatar.url,
+  coverImage:coverImage.url || '',
+  email,
+  password,
+  username:username.toLowerCase()
+})
+
+const createdUser = await User.findById(user._id).select(
+  "-password -refreshToken"
+)
+
+if(!createdUser){
+  throw new ApiError(500,"error while regstering")
+}
+
+return res.status(201).json(new ApiResponse(200,createdUser,"user created successfully!"))
 });
 
 export { registerUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,7 +53,7 @@ const userSchema = new Schema(
 // ecrypting password
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   //   only whenn passrword field used
   next();
 });
