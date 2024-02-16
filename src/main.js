5fb038f62afae71e9b0ab059a7ac8a62a7056f8b
mohan13 <mohangrg250@gmail.com>
2024-02-16 08:48:11 +0545

    set up route and tested in postman

--- src/main.js
+++ src/main.js
@@ -12,8 +12,14 @@ dotenv.config({
 app.use(cors({ origin: '*' }));
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
 app.use(cookieParser());
 
+//routes import
+import userRouter from './routes/user.routes.js';
+
+//routes declaration
+app.use('/api/v1/users', userRouter);
 // mongodb connection
 
 connectDB()
